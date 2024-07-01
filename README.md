c8d8667d483ab896a067f89ae6bf22248b46527d
AI PR <ai_pr@test.com>
2024-07-01 03:06:42 +0000

    Suggestion

--- README.md
+++ README.md
@@ -1,25 +1,24 @@
 # TicTacToe
 
-A simple Python implementation of the classic Tic-Tac-Toe game.
+A simple Python implementation of the classic Tic-Tac-Toe game with a graphical user interface using Tkinter.
 
 ## Features
 
 - Two-player gameplay
-- Player name input
-- ASCII art game board
+- Graphical user interface
 - Win detection
 - Tie detection
 
 ## How to Play
 
 1. Run the `main.py` file
-2. Enter names for Player X and Player O
-3. Players take turns entering row and column numbers to place their mark
-4. The game ends when a player wins or the board is full (tie)
+2. Players take turns clicking on empty cells to place their mark (X or O)
+3. The game ends when a player wins or the board is full (tie)
 
 ## Requirements
 
 - Python 3.x
+- Tkinter (usually comes pre-installed with Python)
 
 ## Running the Game
 
@@ -27,4 +26,4 @@ A simple Python implementation of the classic Tic-Tac-Toe game.
 python main.py
 ```
 
-Enjoy playing Tic-Tac-Toe!
+Enjoy playing Tic-Tac-Toe with a graphical interface!
--- main.py
+++ main.py
@@ -1,88 +1,52 @@
-# Tic Tac Toe game v6 in Python
-
-def print_title():
-    print("""
-    _____ _     _____         _____
-   \|_   _(_)   \|_   _\|_ _  __\|_   _\|__   ___
-     \| \| \| \|     \| \|/ _` \|/ __\|\| \|/ _ \ / _      \| \| \| \|     \| \| (_\| \| (__ \| \| (_) \|  __/
-     \|_\| \|_\|     \|_\|\__,_\|\___\|\|_\|\___/ \___\|
-    """)
+import tkinter as tk
+from tkinter import messagebox
 
 
-def print_board(board):
-    print("""
-       0   1   2
-     ┌───┬───┬───┐
-   0 │ {} │ {} │ {} │
-     ├───┼───┼───┤
-   1 │ {} │ {} │ {} │
-     ├───┼───┼───┤
-   2 │ {} │ {} │ {} │
-     └───┴───┴───┘
-    """.format(*[cell for row in board for cell in row]))
+class TicTacToe:
+    def __init__(self, master):
+        self.master = master
+        self.master.title('Tic Tac Toe')
+        self.current_player = 'X'
+        self.board = [[' ' for _ in range(3)] for _ in range(3)]
+        self.buttons = [[None for _ in range(3)] for _ in range(3)]
+
+        for i in range(3):
+            for j in range(3):
+                self.buttons[i][j] = tk.Button(master, text=' ', font=('normal', 20), width=5, height=2,
+                                               command=lambda row=i, col=j: self.on_click(row, col))
+                self.buttons[i][j].grid(row=i, column=j)
+
+    def on_click(self, row, col):
+        if self.board[row][col] == ' ':
+            self.board[row][col] = self.current_player
+            self.buttons[row][col].config(text=self.current_player)
+            if self.check_winner():
+                messagebox.showinfo(
+                    'Game Over', f'Player {self.current_player} wins!')
+                self.master.quit()
+            elif self.is_full():
+                messagebox.showinfo('Game Over', 'It\'s a tie!')
+                self.master.quit()
+            else:
+                self.current_player = 'O' if self.current_player == 'X' else 'X'
+
+    def check_winner(self):
+        for i in range(3):
+            if self.board[i][0] == self.board[i][1] == self.board[i][2] != ' ':
+                return True
+            if self.board[0][i] == self.board[1][i] == self.board[2][i] != ' ':
+                return True
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] != ' ':
+            return True
+        if self.board[0][2] == self.board[1][1] == self.board[2][0] != ' ':
+            return True
+        return False
+
+    def is_full(self):
+        return all(cell != ' ' for row in self.board for cell in row)
 
 
-def check_winner(board):
-    # Check rows, columns, and diagonals
-    for i in range(3):
-        if board[i][0] == board[i][1] == board[i][2] != ' ':
-            return board[i][0]
-        if board[0][i] == board[1][i] == board[2][i] != ' ':
-            return board[0][i]
-    if board[0][0] == board[1][1] == board[2][2] != ' ':
-        return board[0][0]
-    if board[0][2] == board[1][1] == board[2][0] != ' ':
-        return board[0][2]
-    return None
-
-
-def is_full(board):
-    return all(cell != ' ' for row in board for cell in row)
-
-
-def play_game():
-    board = [[' ' for _ in range(3)] for _ in range(3)]
-    player_x = input("Enter name for Player X: ")
-    player_o = input("Enter name for Player O: ")
-    current_player = 'X'
-    current_name = player_x
-
-    print_title()
-    while True:
-        print_board(board)
-        row = int(
-            input(f"{current_name} ({current_player}), enter row (0-2): "))
-        col = int(
-            input(f"{current_name} ({current_player}), enter column (0-2): "))
-
-        if board[row][col] == ' ':
-            board[row][col] = current_player
-            winner = check_winner(board)
-            if winner:
-                print_board(board)
-                print(f"{current_name} ({winner}) wins!")
-                print("""
-                 ___________
-                '._==_==_=_.'
-                .-\:      /-.
-               \| (\|:.     \|) \|
-                '-\|:.     \|-'
-                  \::.    /
-                   '::. .'
-                     ) (
-                   _.' '._
-                  `"""""""`
-                """)
-                break
-            elif is_full(board):
-                print_board(board)
-                print("It's a tie!")
-                break
-            current_player = 'O' if current_player == 'X' else 'X'
-            current_name = player_o if current_player == 'O' else player_x
-        else:
-            print("That cell is already occupied. Try again.")
-
-
-if __name__ == "__main__":
-    play_game()
+if __name__ == '__main__':
+    root = tk.Tk()
+    game = TicTacToe(root)
+    root.mainloop()
